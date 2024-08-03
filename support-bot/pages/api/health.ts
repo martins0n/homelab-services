1b3622b35916e98a7e04119c2282df8d0f791278
martins0n <33594071+martins0n@users.noreply.github.com>
2024-08-03 16:09:53 +0400

    fix: cron

--- support-bot/pages/api/health.ts
+++ support-bot/pages/api/health.ts
@@ -1,6 +1,7 @@
-import cron from '../../cron';
+import messageRepository from "../../storage/messageRepository"
 
 
 export default async (req, res) => {
+    const _ = await messageRepository.ping();
     return res.status(200).json({ status: 'Healthy' })
 }
