1edbde22c2869f849bfdb5801a77c8416a2dd4d0
martins0n <33594071+martins0n@users.noreply.github.com>
2024-08-03 12:23:52 +0400

    new: cron ping db

---
+++ support-bot/cron.js
@@ -0,0 +1,13 @@
+import cron from 'node-cron';
+import messageRepository from './storage/messageRepository';
+
+
+// Schedule tasks to be run on the server.
+cron.schedule('*/30 * * * *', () => {
+    console.log('Ping databse');
+
+    messageRepository.ping().then((res) => {
+        console.log('Ping database success');
+    })
+
+});
